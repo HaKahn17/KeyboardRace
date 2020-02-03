43da367a926bb65a0d2a8bf2b52aa267da32c42f
Liam Liden <l14m@msn.com>
2020-02-03 13:47:38 +0100

    add obstacle random length

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ObstacleController : MonoBehaviour
 {
     public int length;
+    public float obstacleMaxLength;
     public double timePerObstacle;
     public GameObject bottomObstacle;
     public GameObject topObstacle;
@@ -23,6 +24,7 @@ public class ObstacleController : MonoBehaviour
         if (timer <= 0)
         {
             Vector3 obsPos;
+            GameObject obs = null;
             int rand = Random.Range(0, 2);
 
             switch (rand)
@@ -30,14 +32,18 @@ public class ObstacleController : MonoBehaviour
                 // Bottom
                 case 0:
                     obsPos = new Vector3(transform.position.x, transform.position.y - length);
-                    Instantiate(bottomObstacle, obsPos, Quaternion.identity);
+                    obs = Instantiate(bottomObstacle, obsPos, Quaternion.identity);
                     break;
                 // Top
                 case 1:
                     obsPos = new Vector3(transform.position.x, transform.position.y + length);
-                    Instantiate(topObstacle, obsPos, Quaternion.identity);
+                    obs = Instantiate(topObstacle, obsPos, Quaternion.identity);
                     break;
             }
+
+            // Random length
+            if (obs != null)
+                obs.transform.localScale = new Vector3(Random.Range(1f, obstacleMaxLength), 2, 0);
             
             timer = timePerObstacle;
         }
