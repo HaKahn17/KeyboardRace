bfab0e2873f08c4256c213f21665c92377a04854
cokayc <mselvag1@swarthmore.edu>
2020-02-03 12:25:10 +0100

    added player jumping back

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour {
+    public float jumpSpeed;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded()) 
+        {
+            rb2d.velocity = Vector2.up * jumpSpeed;
+        }
+    }
+
+    bool IsGrounded()
+    {
+        var hit = Physics2D.Raycast(transform.position, -Vector2.up, 1f, groundLayer);
+        return hit.collider != null;
+    }
+}
