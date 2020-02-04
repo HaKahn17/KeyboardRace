715d7f18da30a1ac21009728731c9af2f8fcee57
Haakon Larsen <haakon.g.larsen@outlook.com>
2020-02-04 14:01:38 +0100

    bomb functionality

--- Assets/Bomb.cs
+++ Assets/Bomb.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class Bomb : Powerup
 {
 
+    private PlayerController pc = null;
+
+    private SpriteRenderer sr;
+
+    private int playerNumber;
+
+    private float gravity;
     public GameObject splode;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public override void Effect(GameObject player)
     {
+        if (pc != null)
+            return;
+        pc = player.GetComponent<PlayerController>();
+        playerNumber = pc.playerNum;
+        gravity = pc.GetComponent<Rigidbody2D>().gravityScale;
+        pc.GetComponent<Rigidbody2D>().gravityScale = 0;
+           
+        sr.enabled = false;
         StartCoroutine(Timer(duration));
     }
 
     public override IEnumerator Timer(float dur)
     {
-        Instantiate(splode, transform.position, Quaternion.identity);
+        var obj=Instantiate(splode, transform.position, Quaternion.identity);
         yield return new WaitForSeconds(dur);
+        pc.GetComponent<Rigidbody2D>().gravityScale = gravity;
+        Destroy(obj);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -77,7 +77,7 @@ public class PlayerController : MonoBehaviour {
         */
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.gameObject.CompareTag("Despawn")&&!isDead)
         {
