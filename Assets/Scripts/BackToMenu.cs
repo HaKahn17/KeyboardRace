eba42068b54acbd290da7f935d56f6e505f24270
cokayc <mselvag1@swarthmore.edu>
2020-02-04 12:24:32 +0100

    changed victory screen sprites

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    void Start() {
+        StartCoroutine("Reload");
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")) {
+            SceneManager.LoadScene("Title");
+        }
+    }
+
+    public IEnumerator Reload() {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("Title");  
+    }
+}
