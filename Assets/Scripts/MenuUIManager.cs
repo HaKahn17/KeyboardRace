c1ec9d60f7e8e3cf64223a122d8cc13f9a19f7af
cokayc <mselvag1@swarthmore.edu>
2020-02-04 11:18:43 +0100

    added players to title screen

--- Assets/Scripts/MenuUIManager.cs
+++ Assets/Scripts/MenuUIManager.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuUIManager : MonoBehaviour {
 
+    public GameObject Player3;
+    public GameObject Player4;
+
     private GameManager gm;
     public void Start()
     {
+        Player3.SetActive(false);
+        Player4.SetActive(false);
+
         gm = GameManager.instance;
     }
     public void loadLevel(string level) {
@@ -21,5 +27,15 @@ public class MenuUIManager : MonoBehaviour {
     public void SetNumPlayers(float val)
     {
         gm.SetNumPlayers(val);
+        if (val > 2) {
+            Player3.SetActive(true);
+        } else {
+            Player3.SetActive(false);
+        }
+        if (val > 3) {
+            Player4.SetActive(true);
+        } else {
+            Player4.SetActive(false);
+        }
     }
 }
